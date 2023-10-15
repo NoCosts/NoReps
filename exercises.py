42cc52dc74eafe59049cf95b63c4bf59180ade36
NoCosts <NoCost@duck.com>
2023-10-15 13:04:14 +0300

    minuend and subtrahend find (by difference value) function added

--- exercises.py
+++ exercises.py
@@ -1,7 +1,7 @@
 import random
 
 
-def find_summands(summ_value):
+def find_summands(summ_value: int) -> str:
     """
     Generates a string representation of the summands that add up to the given summ_value.
     Parameters:
@@ -10,7 +10,7 @@ def find_summands(summ_value):
         str: A string representation of the summands and the sum in the format 'b + r = s',
              where b is the second summand, r is the first summand, and s is the summ_value.
     Example:
-        >>> find_summands(12)
+        >> find_summands(12)
         '5 + 7 = 12'
     """
     s = str(summ_value)
@@ -20,4 +20,20 @@ def find_summands(summ_value):
     return str(b) + ' + ' + str(r) + ' = ' + s
 
 
-print(find_summands(5261))
+def find_minuend_and_subtrahend(difference_value: int) -> str:
+    """
+    Generate a string representing a subtraction problem with a given difference value.
+    Args:
+        difference_value (int): The difference value for the subtraction problem.
+    Returns:
+        str: A string representing the subtraction problem in the format "minuend - subtrahend = difference".
+    """
+    d = str(difference_value)
+    l = len(d)
+    s = random.randint(10 ** (l - 1), (10 ** l) - 1)
+    m = s + difference_value
+    return str(m) + ' - ' + str(s) + ' = ' + d
+
+
+print(find_summands(526))
+print(find_minuend_and_subtrahend(526))
